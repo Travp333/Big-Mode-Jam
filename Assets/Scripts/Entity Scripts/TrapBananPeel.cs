84408088644641d58daba5636802e9c06ab3e09c
jefullmer <james.e.fullmer@gmail.com>
2023-12-02 01:30:05 -0700

    Worked more on traps and imported TMP
    Filled out the entity trap script and banana peel trap with basic trap logic

--- Assets/Scripts/Entity Scripts/TrapBananPeel.cs
+++ Assets/Scripts/Entity Scripts/TrapBananPeel.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapBananPeel : MonoBehaviour
+public class TrapBananPeel : EntityTrap
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
+        base.Update();
+    }
+
+    public override bool ActivateTrap(GameObject triggeredTrap)
+    {
+        if (!base.ActivateTrap(triggeredTrap))
+            return false;
         
+        Destroy(triggeredTrap);
+
+        return true;
     }
 }
