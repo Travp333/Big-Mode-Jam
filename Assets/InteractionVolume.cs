c96ce38d93c86ba51464f52ee7ceedbf2890fbd2
Travp333 <72285478+Travp333@users.noreply.github.com>
2023-12-15 20:07:31 -0800

    More tweaks, added killplane

--- Assets/InteractionVolume.cs
+++ Assets/InteractionVolume.cs
@@ -6,19 +6,31 @@ public class InteractionVolume : MonoBehaviour
 {
 	[SerializeField]
 	GameObject[] trigger;
+	[SerializeField]
+	GameObject killplane;
+	[SerializeField]
+	Transform newPos;
 	// OnTriggerEnter is called when the Collider other enters the trigger.
 	protected void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.tag == "Player"){
 			Debug.Log("HIT player");
-			foreach(GameObject g in trigger){
-				if(g.GetComponent<ConveyorBelt>() != null){
-					g.GetComponent<ConveyorBelt>().EnableBelt();
+			if(trigger != null){
+				foreach(GameObject g in trigger){
+					if(g!= null){
+						if(g.GetComponent<ConveyorBelt>() != null){
+							g.GetComponent<ConveyorBelt>().EnableBelt();
+						}
+						if(g.GetComponent<Interactable>() != null){
+							Debug.Log("DOOR!!");
+							g.GetComponent<Interactable>().OpenDoor = true;
+							Destroy(this.gameObject);
+						}
+					}
+					
 				}
-				if(g.GetComponent<Interactable>() != null){
-					Debug.Log("DOOR!!");
-					g.GetComponent<Interactable>().OpenDoor = true;
-					Destroy(this.gameObject);
+				if(killplane != null){
+					killplane.GetComponent<Killplane>().resetSpawn(newPos);
 				}
 			}
 
